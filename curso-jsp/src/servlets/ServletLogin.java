8bcc4c275f835a7de270dc0ecbdd2152ddb04de7
Wolter <Wolter@DESKTOP-KPTQ5AE>
2023-03-10 22:46:46 -0300

    POM XML?

--- curso-jsp/src/servlets/ServletLogin.java
+++ curso-jsp/src/servlets/ServletLogin.java
@@ -11,56 +11,61 @@ import javax.servlet.http.HttpServletResponse;
 
 import model.ModelLogin;
 
-@WebServlet(urlPatterns = {"/principal/ServletLogin", "/ServletLogin"})
-public class ServletLogin extends HttpServlet {
 
+/*O chamando Controller são as servlets ou ServletLoginController*/
+@WebServlet(urlPatterns = {"/ServletLogin", "/principal/ServletLogin"}) /*Mapeamento de URL que vem da tela*/
+public class ServletLogin extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
 
-	public ServletLogin() {
+  
+    public ServletLogin() {
+    }
+
+
+    /*Recebe os dados pela url em parametros*/
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 	}
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	
+	/*recebe os dados enviados por um formulario*/
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		String login = request.getParameter("login");
 		String senha = request.getParameter("senha");
 		String url = request.getParameter("url");
-
+		
+		
 		if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
+			
 			ModelLogin modelLogin = new ModelLogin();
 			modelLogin.setLogin(login);
-			modelLogin.setLogin(senha);
-
-			if (modelLogin.getLogin().equalsIgnoreCase("admin") && modelLogin.getSenha().equalsIgnoreCase("admin")) {
-
-				request.getSession().setAttribute("usuario", modelLogin.getLogin());
+			modelLogin.setSenha(senha);
+			
+			if (modelLogin.getLogin().equals("admin") 
+					&& modelLogin.getSenha().equals("admin")) {/*Deu certo o  login*/
 				
-				if(url ==null || url.equals("null")) {
+				request.getSession().setAttribute("usuario", modelLogin);/*Coloca o user na sessao*/
+				
+				if (url == null || url.equals("null")) {
 					url = "principal/principal.jsp";
 				}
-
-				RequestDispatcher redirecionar = request.getRequestDispatcher("url");
+				RequestDispatcher redirecionar = request.getRequestDispatcher(url);
 				redirecionar.forward(request, response);
-
-			} else {
+				
+			}else {
 				RequestDispatcher redirecionar = request.getRequestDispatcher("/index.jsp");
-				request.setAttribute("msg", "Login ou senha incorretos");
+				request.setAttribute("msg", "Informe o login e senha corretamente!");
 				redirecionar.forward(request, response);
-
 			}
-
-		} else {
+			
+		}else {
 			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
-			request.setAttribute("msg", "Login ou senha incorretos");
+			request.setAttribute("msg", "Informe o login e senha corretamente!");
 			redirecionar.forward(request, response);
 		}
-
+		
 	}
 
 }
